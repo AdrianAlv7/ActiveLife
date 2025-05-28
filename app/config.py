3543b3962b44547ae26286148a6b5ee7caa315fe
AdrianAlv7 <149105416+AdrianAlv7@users.noreply.github.com>
2025-05-28 12:41:18 -0600

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 class Config:
-    MYSQL_HOST = 'gondola.proxy.rlwy.net'
+    MYSQL_HOST = 'mysql.railway.internal'
     MYSQL_USER = 'root'
     MYSQL_PASSWORD = 'OurKWWLGcMXAZAHUMkotlTicTRePkkwu'
     MYSQL_DB = 'activelife_db'
-    MYSQL_PORT = 37480
+    MYSQL_PORT = 3306
 
